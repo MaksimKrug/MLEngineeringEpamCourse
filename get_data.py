3fa6a7c3c2cd27b525f9dd6d717918289e593994
Maksim Krug <maksim_krug96@mail.ru>
2022-08-09 19:04:54 +0200

    black formated

--- get_data.py
+++ get_data.py
@@ -3,4 +3,4 @@ import gdown
 # download a file
 url = "https://drive.google.com/uc?id=1sGu6O1z9lj2cSvk4yrJJ4aftiWtSfTwj"
 output = "data/jigsaw-toxic-comment-train.csv"
-gdown.download(url, output, quiet=False)
+gdown.download(url, output, quiet=False) 
--- train.py
+++ train.py
@@ -42,11 +42,12 @@ acc, f1 = accuracy_score(y_test, y_pred), f1_score(y_test, y_pred, average="micr
 with open("metrics.json", "w") as f:
     json.dump({"accuracy": acc, "f1_score": f1}, f)
 
-
 # get the most important words
 all_words = vectorizer.get_feature_names()
 words_importnaces = model.feature_importances_
-the_most_important_words = sorted(list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True) 
+the_most_important_words = sorted(
+    list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True
+)
 temp_data = pd.DataFrame(the_most_important_words, columns=["Words", "Importance"])
 fig = sns.barplot(x="Words", y="Importance", data=temp_data[:10])
-fig.get_figure().savefig("feature_importances.png") 
+fig.get_figure().savefig("feature_importances.png")
