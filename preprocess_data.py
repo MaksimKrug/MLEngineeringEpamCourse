4f4d0a0b69f9b7852f8be6f7aaf6a6f48a398c7f
Maksim Krug <maksim_krug96@mail.ru>
2022-08-23 17:09:41 +0200

    temp

--- preprocess_data.py
+++ preprocess_data.py
@@ -12,16 +12,14 @@ df = pd.read_csv("data/jigsaw-toxic-comment-train.csv", index_col=0)
 
 # preprocess text
 df["comment_text"] = df["comment_text"].apply(lambda x: x.lower().replace("\n", " "))
-df["comment_text"] = df["comment_text"].apply(lambda x: tokenizer.tokenize(x)) 
+df["comment_text"] = df["comment_text"].apply(lambda x: tokenizer.tokenize(x))
 df["comment_text"] = df["comment_text"].apply(
-    lambda x: " ".join([lemmatizer.lemmatize(w) for w in x]) 
+    lambda x: " ".join([lemmatizer.lemmatize(w) for w in x])
 )
 
 # remove empty texts
-df = df.loc[df["comment_text"].notna()] 
-df = df.loc[df["comment_text"] != ""] 
+df = df.loc[df["comment_text"].notna()]
+df = df.loc[df["comment_text"] != ""]
 
 # save preprocessed data
-df.to_csv("data/preprocessed_data.csv", index=False) 
-
-print(df["toxic"].value_counts(normalize=True))
+df.to_csv("data/preprocessed_data.csv", index=False)
--- train.py
+++ train.py
@@ -1,52 +1,126 @@
 import json
-import joblib
 
+import joblib
+import mlflow
+import optuna
 import pandas as pd
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
-# load data
-df = pd.read_csv("data/preprocessed_data.csv")
+if __name__ == "__main__":
+    """
+    DATA
+    """
+    # load data
+    df = pd.read_csv("data/preprocessed_data.csv")
+    # split
+    train_data, test_data = train_test_split(df.sample(100), random_state=42)
+    # TF-IDF
+    vectorizer = TfidfVectorizer()
+    X_train = vectorizer.fit_transform(train_data["comment_text"])
+    X_test = vectorizer.transform(test_data["comment_text"])
+    joblib.dump(vectorizer, "vectorizer.joblib")
+    # preprocess target
+    target_columns = [
+        "toxic",
+    ]
+    y_train = train_data[target_columns].values
+    y_test = test_data[target_columns].values
 
-# split
-train_data, test_data = train_test_split(df, random_state=42)
+    """
+    HYPERPARAMETER OPTIMIZATION
+    """
 
-# TF-IDF
-vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(train_data["comment_text"])
-X_test = vectorizer.transform(test_data["comment_text"])
-joblib.dump(vectorizer, "vectorizer.joblib")
+    def objective(trial):
+        # params
+        model_name = trial.suggest_categorical(
+            "model_name", ["RandomForest", "LogisticRegression", "SVC"]
+        )
+        if model_name == "RandomForest":
+            params = {
+                "max_depth": trial.suggest_int("max_depth", 2, 32),
+                "n_estimators": trial.suggest_int("n_estimators", 50, 150),
+                "random_state": 42,
+            }
+            model = RandomForestClassifier(**params)
+        elif model_name == "LogisticRegression":
+            params = {
+                "C": trial.suggest_float("C", 1e-8, 1e2),
+                "penalty": trial.suggest_categorical(
+                    "penalty",
+                    [
+                        "l1",
+                        "l2",
+                    ],
+                ),
+                "solver": "liblinear",
+                "random_state": 42,
+            }
+            model = LogisticRegression(**params)
+        elif model_name == "SVC":
+            params = {
+                "C": trial.suggest_float("C", 1e-8, 1e2),
+                "kernel": trial.suggest_categorical(
+                    "kernel", ["linear", "rbf", "sigmoid"]
+                ),
+                "random_state": 42,
+            }
+            model = SVC(**params)
 
-# preprocess target
-target_columns = [
-    "toxic",
-]
-y_train = train_data[target_columns].values
-y_test = test_data[target_columns].values
+        # train model
+        model.fit(X_train, y_train.ravel())
 
-# train model
-model = RandomForestClassifier(random_state=42, max_depth=10)
-model.fit(X_train, y_train.ravel())
+        # metrics
+        y_pred = model.predict(X_test)
+        metrics = {
+            "accuracy": accuracy_score(y_test, y_pred),
+            "f1": f1_score(y_test, y_pred, average="macro"),
+        }
 
-# metrics
-y_pred = model.predict(X_test)
-acc, f1 = accuracy_score(y_test, y_pred), f1_score(y_test, y_pred, average="macro")
+        # mlflow log
+        with mlflow.start_run() as mlrun:
+            params["model_name"] = model_name
+            mlflow.log_params(params)
+            mlflow.log_metrics(metrics)
 
-with open("metrics.json", "w") as f:
-    json.dump({"accuracy": acc, "f1_score": f1}, f)
+        return metrics["f1"]
 
-# get the most important words
-all_words = vectorizer.get_feature_names_out()
-words_importnaces = model.feature_importances_
-the_most_important_words = sorted(
-    list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True
-)
-temp_data = pd.DataFrame(the_most_important_words, columns=["Words", "Importance"])
-fig = sns.barplot(x="Words", y="Importance", data=temp_data[:10])
-fig.get_figure().savefig("feature_importances.png")
+    # hyperparameters optimization
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=30, timeout=60 * 10)
 
-# save model
-joblib.dump(model, "model.joblib")
+    print(study.best_params)
+
+    # # params
+    # model_name = "RandomForest"
+    # params = {"random_state": 42, "max_depth": 3}
+    # mlflow.log_params(params)
+    # # train model
+    # model = RandomForestClassifier(**params)
+    # model.fit(X_train, y_train.ravel())
+    # mlflow.sklearn.log_model(model, model_name)
+    # """
+    # METRICS
+    # """
+    # # metrics
+    # y_pred = model.predict(X_test)
+    # metrics = {
+    #     "accuracy": accuracy_score(y_test, y_pred),
+    #     "f1": f1_score(y_test, y_pred, average="macro"),
+    # }
+    # mlflow.log_metrics(metrics)
+    # # get the most important words
+    # all_words = vectorizer.get_feature_names_out()
+    # words_importnaces = model.feature_importances_
+    # the_most_important_words = sorted(
+    #     list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True
+    # )
+    # temp_data = pd.DataFrame(the_most_important_words, columns=["Words", "Importance"])
+    # fig = sns.barplot(x="Words", y="Importance", data=temp_data[:10])
+    # fig.get_figure().savefig("feature_importances.png")
+    # mlflow.log_figure(fig.get_figure(), "feature_importances.png")
