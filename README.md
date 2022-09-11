da5e980fe70176a25d298e0cb745aec7beeed96a
Maksim Krug <maksim_krug96@mail.ru>
2022-09-11 17:47:43 +0200

    connected containers

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MLEngineeringEpamCourse (Task 3)
 
+MlFolw Server is here: http://localhost:5000/
 
+```
+docker-compose up -d --build
+docker-compose run --rm train_pipeline
+python3 train.py
+```
--- train_pipeline/train.py
+++ train_pipeline/train.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     # load data
     df = pd.read_csv("data/preprocessed_data.csv")
     # split
-    train_data, test_data = train_test_split(df.sample(100), random_state=42)
+    train_data, test_data = train_test_split(df.sample(10000), random_state=42)
     # TF-IDF
     vectorizer = TfidfVectorizer()
     X_train = vectorizer.fit_transform(train_data["comment_text"])
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             params = {
                 "max_depth": trial.suggest_int("max_depth", 2, 32),
                 "n_estimators": trial.suggest_int("n_estimators", 50, 150),
-                "random_state": 42,
+                "random_state": trial.suggest_categorical("random_state", [42]),
             }
             model = RandomForestClassifier(**params)
         elif model_name == "LogisticRegression":
@@ -58,8 +58,8 @@ if __name__ == "__main__":
                         "l2",
                     ],
                 ),
-                "solver": "liblinear",
-                "random_state": 42,
+                "solver": trial.suggest_categorical("solver", ["liblinear"]),
+                "random_state": trial.suggest_categorical("random_state", [42]),
             }
             model = LogisticRegression(**params)
         elif model_name == "SVC":
@@ -68,7 +68,7 @@ if __name__ == "__main__":
                 "kernel": trial.suggest_categorical(
                     "kernel", ["linear", "rbf", "sigmoid"]
                 ),
-                "random_state": 42,
+                "random_state": trial.suggest_categorical("random_state", [42]),
             }
             model = SVC(**params)
 
@@ -82,9 +82,10 @@ if __name__ == "__main__":
             "f1": f1_score(y_test, y_pred, average="macro"),
         }
 
-        # mlflow log
+        # mlflow log        
+        mlflow.set_tracking_uri("http://mlflow_back:5000/")
         with mlflow.start_run() as mlrun:
-            params["model_name"] = model_name
+            mlflow.set_tag("Model Name", model_name)
             mlflow.log_params(params)
             mlflow.log_metrics(metrics)
 
@@ -92,35 +93,46 @@ if __name__ == "__main__":
 
     # hyperparameters optimization
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=30, timeout=60 * 10)
+    study.optimize(objective, n_trials=10, timeout=60 * 2)
 
+
+    """
+    SAVE THE BEST MODEL
+    """
+    print("Best Params:")
     print(study.best_params)
+    print("-"*20)
 
-    # # params
-    # model_name = "RandomForest"
-    # params = {"random_state": 42, "max_depth": 3}
-    # mlflow.log_params(params)
-    # # train model
-    # model = RandomForestClassifier(**params)
-    # model.fit(X_train, y_train.ravel())
-    # mlflow.sklearn.log_model(model, model_name)
-    # """
-    # METRICS
-    # """
-    # # metrics
-    # y_pred = model.predict(X_test)
-    # metrics = {
-    #     "accuracy": accuracy_score(y_test, y_pred),
-    #     "f1": f1_score(y_test, y_pred, average="macro"),
-    # }
-    # mlflow.log_metrics(metrics)
-    # # get the most important words
-    # all_words = vectorizer.get_feature_names_out()
-    # words_importnaces = model.feature_importances_
-    # the_most_important_words = sorted(
-    #     list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True
-    # )
-    # temp_data = pd.DataFrame(the_most_important_words, columns=["Words", "Importance"])
-    # fig = sns.barplot(x="Words", y="Importance", data=temp_data[:10])
-    # fig.get_figure().savefig("feature_importances.png")
-    # mlflow.log_figure(fig.get_figure(), "feature_importances.png")
+    # params
+    params = study.best_params
+    model_name = params.pop("model_name")    
+    mlflow.log_params(params)
+    mlflow.set_tag("Model Name", model_name)
+    # train model
+    if model_name == "RandomForest":
+        model = RandomForestClassifier(**params)
+    elif model_name == "LogisticRegression":
+        model = LogisticRegression(**params)
+    elif model_name == "SVC":
+        model = SVC(**params)
+    model.fit(X_train, y_train.ravel())
+    mlflow.sklearn.log_model(model, model_name)
+    
+    # metrics
+    y_pred = model.predict(X_test)
+    metrics = {
+        "accuracy": accuracy_score(y_test, y_pred),
+        "f1": f1_score(y_test, y_pred, average="macro"),
+    }
+    mlflow.log_metrics(metrics)
+    # get the most important words
+    if model_name == "RandomForest":
+        all_words = vectorizer.get_feature_names_out()
+        words_importnaces = model.feature_importances_
+        the_most_important_words = sorted(
+            list(zip(all_words, words_importnaces)), key=lambda x: x[1], reverse=True
+        )
+        temp_data = pd.DataFrame(the_most_important_words, columns=["Words", "Importance"])
+        fig = sns.barplot(x="Words", y="Importance", data=temp_data[:10])
+        fig.get_figure().savefig("feature_importances.png")
+        mlflow.log_figure(fig.get_figure(), "feature_importances.png")
